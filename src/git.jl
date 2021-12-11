84ff092854f7396f1dc73e035ae994dab52600c5
Rik Huijzer <rikhuijzer@pm.me>
2021-12-11 20:44:54 +0100

    Add git email and name

--- src/git.jl
+++ src/git.jl
@@ -46,6 +46,8 @@ function commit!(repo::Repo)
     dir = clone_dir(repo)
     branch = repo.branch
     cd(dir) do
+        run(`git config --global user.email "skanbot@example.com"`)
+        run(`git config --global user.name "skanbot"`)
         run(`git add .`)
         run(`git commit -m '[Bot] Update stored pages'`)
         # Required for first push on new branch.
